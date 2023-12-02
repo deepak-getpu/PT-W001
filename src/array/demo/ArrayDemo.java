12ac8e65419745a40e23976d248abc53c5eb90a4
deepak-getpu <deepak.getpu@gmail.com>
2023-12-01 23:10:14 -0500

    adding the assignment1

---
+++ src/array/demo/ArrayDemo.java
@@ -0,0 +1,20 @@
+package array.demo;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+
+//	public static void main(String[] args) {
+		
+		
+		public static void main(String[] args) {
+			String[] names = new String[2];
+			names[0] = "Frodo";
+			names[1] = "Trodo";
+					
+			
+					System.out.println(Arrays.toString(names));
+		}
+
+	}
+
---
+++ src/com/promineotech/RowRowRowYourBoat.java
@@ -0,0 +1,21 @@
+package com.promineotech;
+
+import java.util.Arrays;
+
+public class RowRowRowYourBoat {
+
+	public static void main(String[] args) {
+		/*
+		 * Write a program that prints out the lyrics to 
+		 * "Row Row Row Your Boat"
+		 * "Gently down the stream"
+		 */
+
+		String[] names = {"Patrick", "Dawson", "Jessica", "Ranee"};
+		System.out.println(Arrays.toString(names));
+		
+		System.out.println();
+		System.out.println("Row Row Row Your Boat");
+	} //End of main method
+
+}
